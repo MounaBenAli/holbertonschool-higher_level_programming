9825cbd8c4a3703f2b6f46a6c3e7b438289f11af
MounaBenAli <benalimouna1986@gmail.com>
2021-12-21 08:42:23 -0800

    Checker Updates

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number < 10:
+    if number < 99:
         print(str(number).zfill(2), end=', ')
     else:
-        print(number, end=', ')
+        print(number)
