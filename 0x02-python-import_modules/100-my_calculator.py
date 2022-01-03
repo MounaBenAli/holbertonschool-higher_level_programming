a70e4449b75c52a95ce45033b5decc71535b3732
MounaBenAli <benalimouna1986@gmail.com>
2022-01-03 10:23:58 -0800

    "Tasks 1.2.3"

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+if argc != 3:
+print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
+exit(1)
+operator = "+ - * /"
