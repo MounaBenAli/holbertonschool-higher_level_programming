4dc28f3af05dc0814e639c8b7aa74514749949ab
MounaBenAli <benalimouna1986@gmail.com>
2022-01-19 15:23:52 +0100

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+from re import X
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
