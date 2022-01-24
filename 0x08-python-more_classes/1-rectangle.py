9949203cd5f55b933b7712021cdf7f5a8e8409e3
MounaBenAli <benalimouna1986@gmail.com>
2022-01-24 18:32:37 +0100

    "Tasks 0-1"

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Define a class Rectangle ."""
+
+
+class Rectangle:
+    """Represents a square
+
+    Attributes: __width + __height
+    """
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+        
+
+    @property
+    def height(self):
+        """retreiver or getter property for __height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter property for __height."""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        else:
+            self.__height = value
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+    @property
+    def width(self):
+        """retreiver or getter property for __width."""
+        return self.__width
+        
+    @width.setter
+    def width(self, value):
+        """setter property for __width."""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        else:
+            self.__width = value
+        if value < 0:
+            raise ValueError('width must be >= 0')
+    
+   
+    
+       
