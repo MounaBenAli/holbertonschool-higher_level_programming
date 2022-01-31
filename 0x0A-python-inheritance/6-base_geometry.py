3576ea8abde422e0e398a5fa50bd81961059a00c
MounaBenAli <benalimouna1986@gmail.com>
2022-01-31 18:21:05 +0100

    "pycodestyle"

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Define a base geomtry class BaseGeometry."""
 
+
 class BaseGeometry:
     """Represent base geometry"""
 
     def area(self):
-    """Not implemented"""
+        """Not implemented"""
         raise Exception("area() is not implemented")
-
