6f54fccb3f39e3cf5ca06ae918ba3aff7ea75b5e
MounaBenAli <benalimouna1986@gmail.com>
2022-01-31 19:42:28 +0100

    "Task 12"

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class MyInt(int):
+    """The inheritance from int class.
+    MyInt has == and != operators inverted
+    """
+
+    def __eq__(self, other):
+        """eq magic method"""
+        if isinstance(other, int):
+            return False
+
+    def __ne__(self, other):
+        """ne magic method"""
+        if isinstance(other, int):
+            return True
