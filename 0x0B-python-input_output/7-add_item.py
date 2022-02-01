80fbb0e286afa98684c14123d4a821474efb4488
MounaBenAli <benalimouna1986@gmail.com>
2022-02-01 19:52:46 +0100

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Load Add Save Module"""
+
+
+import json
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+filename = "add_item.json"
+args = sys.argv[1:]
+with open(filename, "a") as f:
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+
+    my_list.extend(args)
+    save_to_json_file(my_list, filename)
