67d2c712e587a5703146d03f3a366e80f60b5f78
MounaBenAli <benalimouna1986@gmail.com>
2022-02-01 20:09:05 +0100

    "pycodestyle"

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Load Add Save Module"""
 
-import json
+
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
