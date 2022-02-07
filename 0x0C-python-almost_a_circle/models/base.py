fba78112644c3be589b34af8a98c3bbe8cca8be3
MounaBenAli <benalimouna1986@gmail.com>
2022-02-07 22:52:56 +0100

    json to string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,21 @@ class Base():
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        dict = []
+        if list_objs is None:
+            return dict
+        with open(filename, "w") as f:
+            json_rep = cls.to_json_string(dict)
+            f.write(json_rep)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
