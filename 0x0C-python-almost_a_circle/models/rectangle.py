82450f7ea947bace22f24b2cad601862e79b9f66
MounaBenAli <benalimouna1986@gmail.com>
2022-02-07 14:56:31 +0100

    fixing Attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,10 +16,10 @@ class Rectangle(Base):
         y (int): The y coordinate of the new Rectangle.
         id (int): The identity of the new Rectangle.
         """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
