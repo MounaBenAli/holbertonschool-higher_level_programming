6d492b3857ac8247c61a7678b52b60b7ee3df3db
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 17:59:29 +0100

    fix

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 
 -- The database name will be passed as an argument of the mysql command
 INSERT INTO first_table (`id`, `name`) VALUES(89, 'Best School');
