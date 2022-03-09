3040bc38d270e4333ebfd56f3a6f65ff22fc9195
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 20:51:09 +0100

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,7 +5,8 @@
 -- If the table states already exists, your script should not fail
 CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE states (
+CREATE TABLE 
+    IF NOT EXISTS states@hbtn_0d_usa (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (Personid)
