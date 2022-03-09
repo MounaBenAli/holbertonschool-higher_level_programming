7367c676a954fca660053ecc6d6430d32fa5f48b
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 23:30:46 +0100

    fix

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -6,7 +6,6 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title , tv_show_genres.genre_id
 FROM tv_shows
-WHERE tv_show_genres.genre_id IS NULL
-INNER JOIN tv_show_genres 
+JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
