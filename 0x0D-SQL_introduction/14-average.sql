591c52240f6589e0c63a5b93c20e6882506e85e0
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 18:35:46 +0100

    fix

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- computes the score average of all records in the table second_table of the database hbtn_0c_0 
 -- The result column name should be average
-SELECT AVG(*)
+SELECT AVG(score) as average 
 FROM second_table;
