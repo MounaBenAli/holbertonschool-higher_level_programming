d65489b7b9d66e44be019e6e6d219c30c1832f4e
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 22:23:27 +0100

    fix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -11,6 +11,6 @@ CREATE TABLE
     id INT NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
