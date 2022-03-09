6b6439a2a2d08fa75805f578a14c26638138f7c7
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 18:50:36 +0100

    fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score in the table second_table of the database hbtn_0c_0
---the number of records for this score with the label number
---The list should be sorted by the number of records (descending)
+-- the number of records for this score with the label number
+-- The list should be sorted by the number of records (descending)
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
