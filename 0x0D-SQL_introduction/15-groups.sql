5519748ae607fc36eaefed2ba183c984698902a6
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 18:47:06 +0100

    fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -3,8 +3,4 @@
 -- The result should display: the score
 --the number of records for this score with the label number
 --The list should be sorted by the number of records (descending)
-
-SELECT score, COUNT(score) AS number 
-FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC
