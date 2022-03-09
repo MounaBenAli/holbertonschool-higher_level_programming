b1c8c0050962449ae4fb2cd2d1381e2c50075640
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 19:03:50 +0100

    Task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,4 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER `name` IN first_table CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
