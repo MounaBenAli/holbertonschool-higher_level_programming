7d9f37c6fe4cbdc5a45766e00145e9733cfb8947
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 23:11:58 +0100

    fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -4,4 +4,4 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = state.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;
