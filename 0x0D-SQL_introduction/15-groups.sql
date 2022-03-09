94922e4ee7c8547b7cd714b6d0281c465039167a
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 18:46:12 +0100

    fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -4,7 +4,7 @@
 --the number of records for this score with the label number
 --The list should be sorted by the number of records (descending)
 
-SELECT `score`, COUNT(score) AS `number` 
+SELECT score, COUNT(score) AS number 
 FROM second_table
-GROUP BY `score`
-ORDER BY `number`DESC;
+GROUP BY score
+ORDER BY number DESC;
