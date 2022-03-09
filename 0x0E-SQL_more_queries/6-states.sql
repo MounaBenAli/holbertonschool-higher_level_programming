6cad2124e70f049e30e960cec08d9fbad1e099fc
MounaBenAli <benalimouna1986@gmail.com>
2022-03-09 20:51:36 +0100

    Task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,7 +6,7 @@
 CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE 
-    IF NOT EXISTS states@hbtn_0d_usa (
+    IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (Personid)
