2961fccb136bf53b58926a79c2b49ce37dc8a88e
MounaBenAli <benalimouna1986@gmail.com>
2022-04-04 16:50:29 +0200

    prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+//  prints x times “C is fun”
+
+const times = process.argv.slice(2);
+let i = 0;
+const str = 'c is fun';
+
+while (i < times) {
+  i += 1;
+  console.log(str);
+}
+
+if (parseInt(times)) {
+  while (i < times) {
+    i += 1;
+    console.log(str);
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
