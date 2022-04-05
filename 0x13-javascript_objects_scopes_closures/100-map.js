3a766abb28b72ad399cf3e5948a8e2d8fa041646
MounaBenAli <benalimouna1986@gmail.com>
2022-04-05 16:19:11 +0200

    import list from file and computes a new list

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,11 +2,11 @@
 // imports an array and computes a new array
 
 let list = require('./100-data.js').list;
+console.log(list);
 
 let newArr = [];
 for (let i = 0; i < list.length; i++) {
   newArr = list.map((x, i) => x * i);
 }
-
-console.log(list);
 console.log(newArr);
+
