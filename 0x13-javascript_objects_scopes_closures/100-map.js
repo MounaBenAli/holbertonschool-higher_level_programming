56f69176d91c0e7c339f2b7c29613178b6c498da
MounaBenAli <benalimouna1986@gmail.com>
2022-04-05 16:22:25 +0200

    fix semistandard

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // imports an array and computes a new array
 
-let list = require('./100-data.js').list;
+const list = require('./100-data.js').list;
 console.log(list);
 
 let newArr = [];
 newArr = list.map((x, i) => x * i);
 console.log(newArr);
-
