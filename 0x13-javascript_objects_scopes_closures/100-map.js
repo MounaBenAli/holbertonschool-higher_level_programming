c67dc2bbd2e5415fad9eed488f1b9d9474a54dcb
MounaBenAli <benalimouna1986@gmail.com>
2022-04-05 16:13:08 +0200

    using Factor index with map() to compute a new array from imported array

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// imports an array and computes a new array.
+
+const list = require('./100-data.js').list;
+
+let newArr = [];
+for (let i = 0; i < list.length; i++) {
+  newArr = list.map((x, i) => x * i);
+}
+
+console.log(list);
+console.log(newArr);
