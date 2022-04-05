2588a3e3a5a6e5d9384676f7d46eda3b853ce71c
MounaBenAli <benalimouna1986@gmail.com>
2022-04-05 16:16:49 +0200

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // imports an array and computes a new array
 
-const list = require('./100-data.js').list;
+let list = require('./100-data.js').list;
 
 let newArr = [];
 for (let i = 0; i < list.length; i++) {
