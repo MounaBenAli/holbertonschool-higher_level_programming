4dacb064e4dfef97448c68c8e39ba625d69bef9b
MounaBenAli <benalimouna1986@gmail.com>
2022-04-05 13:20:47 +0200

    fix camelCase

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,11 +2,11 @@
 // Write a function that returns the number of occurrences in a list
 
 exports.nbOccurences = function (list, searchElement) {
-  let nb_occ = 0;
+  let numberOccur = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
-      nb_occ += 1;
+      numberOccur += 1;
     }
   }
-  return nb_occ;
+  return numberOccur;
 };
