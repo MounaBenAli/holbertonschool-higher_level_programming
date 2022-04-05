892e88cbcb90751a613a30649ee0fb093bcf38fb
MounaBenAli <benalimouna1986@gmail.com>
2022-04-05 13:05:32 +0200

    3. Rectangle #3 (with instance method print())

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// same object as in Rectangle #2
+// Create an instance method called print() that prints the rectangle using the character X
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
