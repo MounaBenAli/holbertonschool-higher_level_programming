4270050799dc80ec4b7d840cd60565316f2f3dac
MounaBenAli <benalimouna1986@gmail.com>
2022-04-09 12:33:53 +0200

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa"""
 
-from logging import root
-
 
 if __name__ == "__main__":
     import MySQLdb
@@ -12,14 +10,14 @@ if __name__ == "__main__":
         print("Usage: {:s} <username> <password> <database>".format(argv[0]))
         exit(1)
 
-    root = argv[1]
-    passwd = argv[2]
-    hbtn_0e_0_usa = argv[3]
+    username = argv[1]
+    password = argv[2]
+    dbname = argv[3]
 
     database = MySQLdb.Connect(
-        user=root,
-        passwd='',
-        db='hbtn_0e_0_usa',
+        user=username,
+        pwd=password,
+        db=dbname,
         port=3306)
     cursor = database.cursor()
     cursor.execute("SELECT * FROM states")
