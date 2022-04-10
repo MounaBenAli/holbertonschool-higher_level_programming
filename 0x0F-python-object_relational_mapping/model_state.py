7dc54fcfe9c1fe26805b51014fce8ff9169017f2
MounaBenAli <benalimouna1986@gmail.com>
2022-04-10 23:44:38 +0200

    fix nullable

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,5 +11,5 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128))
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
