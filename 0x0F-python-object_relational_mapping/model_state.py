eb3f7509b932f84cd609993f08ca35a1076cc2cd
MounaBenAli <benalimouna1986@gmail.com>
2022-04-10 23:46:10 +0200

    fix comment requirement

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Class State"""
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
