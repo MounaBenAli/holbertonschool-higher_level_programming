8a1df7ab978f10db21bb0d442ff6ce17070c0af5
MounaBenAli <benalimouna1986@gmail.com>
2022-04-10 18:54:59 +0200

    filter db by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv, exit
+
+    if len(argv) != 5:
+        print("Usage: ./2.py <username> <password> <database> <input>")
+        exit(1)
+
+    username = argv[1]
+    password = argv[2]
+    databasename = argv[3]
+
+    database = MySQLdb.Connect(
+        user=username,
+        passwd=password,
+        db=databasename,
+        port=3306)
+    cursor = database.cursor()
+    cursor.execute("""
+        SELECT * FROM states WHERE states.name LIKE '%{:s}%'
+        ORDER BY states.id ASC
+        """.format(input))
+    states = cursor.fetchall()
+    for row in states:
+        print(row)
+    cursor.close()
+    database.close()
