76b9779b596ed5c038cc127bf9f03cf7114896d8
MounaBenAli <benalimouna1986@gmail.com>
2022-04-22 00:32:16 +0200

    updating with cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#This script takes in a URL and displays all HTTP methods the server will accept.
-curl -i -X OPTIONS "$1"
+#This script takes in a URL & displays all HTTP methods the server will accept.
+curl -i -X OPTIONS "$1" | grep "Allow:"| cut -d ":" -f2
