23070348dd1d194a4bebf2889dbdcb025d2149be
MounaBenAli <benalimouna1986@gmail.com>
2022-04-22 23:43:53 +0200

    By works, I meant 'doesnt work'. Works now..:sos:

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ that takes in a URL,
+ sends a request to the URL
+ and displays the body of the response (decoded in utf-8)."""
+
+from urllib import request, parse
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    url = argv[1]
+    with request.urlopen(url) as response:
+        body = response.read()
+    try:
+        request.urlretrieve(url)
+    except HTTPError as err:
+        print('Error code:'.format(err.code))
