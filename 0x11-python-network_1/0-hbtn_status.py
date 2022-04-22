1f39abb3b11e687887dadb66aa076169e0165802
MounaBenAli <benalimouna1986@gmail.com>
2022-04-22 22:28:35 +0200

    trying to fix the output :boom:

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         print('Body response:')
         print('\t- type: {}'.format(type(body)))
         print('\t- conent: {}'.format(body))
-        print('\t- utf8 content: {}'.format(body.decode('UTF-8')))
+        print('\t- utf8 content: {}'.format(body.decode('utf8', 'strict')))
