6a0907cc5e69fe67a2b3a8aa1ab671775f51b183
MounaBenAli <benalimouna1986@gmail.com>
2022-04-22 23:50:34 +0200

    I expected something different.:soon:

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,9 +12,8 @@ from sys import argv
 if __name__ == "__main__":
 
     url = argv[1]
-    with request.urlopen(url) as response:
-        body = response.read()
     try:
-        request.urlretrieve(url)
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf8', 'strict'))
     except HTTPError as err:
-        print('Error code:'.format(err.code))
+        print('Error code: {}'.format(err.code))
