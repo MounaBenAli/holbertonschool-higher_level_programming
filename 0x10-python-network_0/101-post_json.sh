d400589fbeb34f0d57d6b50401fc811ea9e3b445
MounaBenAli <benalimouna1986@gmail.com>
2022-04-26 22:06:56 +0200

    Is there an award for this? :kissing_closed_eyes:

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 
-curl -X POST -H "Content-Type: application/json" -d "@$2" "$1"
+curl -d "@$2" -H "Content-Type: application/json" -X POST "$1"
