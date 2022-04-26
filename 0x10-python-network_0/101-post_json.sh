754cb2b74b05b34e01ca0a75ad30d944b4af2e9b
MounaBenAli <benalimouna1986@gmail.com>
2022-04-26 22:11:27 +0200

    Now every args is in the right place for real :alien:

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 
-curl -d "@$2" -H "Content-Type: application/json" -X POST "$1"
+curl -X POST "$1" -H 'Content-Type: application/json' -d "@$2"
