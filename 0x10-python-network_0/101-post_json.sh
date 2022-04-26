8d11bc76940e2ab1ee1569cd31c810d1d5975ca6
MounaBenAli <benalimouna1986@gmail.com>
2022-04-26 22:17:15 +0200

    guess why this was not working!! lol cause all scripts should be exactly 3 lines long so I was failling the wc -l I need coffee :coffee:

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-
 curl -X POST -H "Content-Type: application/json" -d @"$2" "$1"
